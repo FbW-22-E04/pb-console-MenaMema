1eb99ba8bd4bf658a2e9ad88f43037c1363e501a
MenaMema <amenabarrera@gmail.com>
2022-06-30 11:59:15 +0200

    exercise finished

--- solution.js
+++ solution.js
@@ -1,1 +1,28 @@
 // write your code in this file
+
+console.log("hello");
+
+const exercise = "this is a value";
+console.log(exercise);
+
+let firstName = "Antonio";
+let lastName = "Mena Barrera";
+let city = "Leipzig";
+let job = "Student";
+let age = "29";
+
+console.log(firstName, lastName, city, job, age);
+
+console.log(
+  firstName +
+    " " +
+    lastName +
+    " lives in " +
+    city +
+    " is a " +
+    job +
+    " and has " +
+    age
+);
+
+console.log(firstName, "is", lastName);
